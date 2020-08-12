7bd3e7c2c7746284f6d3b982dc35021b8011ee5c
JohannesWalter <50334143+JohannesWalter@users.noreply.github.com>
2020-08-12 16:15:12 +0200

    asfasf

--- Linear_Regression.jl
+++ Linear_Regression.jl
@@ -1,10 +1,31 @@
 #= 
 This is my implementation of Linear Regression in Julia =#
 
-if true
-    print("Hello")
+# How do I get to my conde?
+# Is there a request package in Julia?
+# Let's find out!
+
+# using Pkg 
+# Pkg.add("HTTP")
+
+using HTTP
+
+response = HTTP.request("GET", "https://raw.githubusercontent.com/yashLadha/The_Math_of_Intelligence/master/Week1/ADRvsRating.csv")
+
+println(response.status)
+println(String(response.body))
+typeof(response.body)
+typeof(String(response.body))
+r = String(response.body)
+lines = split(r, "\n")
+
+data = []
+
+for item in lines
+    split(item, ",")
+    append!(data, item)
 end
 
-x = false
-if x || true
-    print("this is alwys true")
+print("Everything went well")
+typeof(data)
+
