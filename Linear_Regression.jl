605c2366484dfcd447a0cce53ed09a3e919707ac
JohannesWalter <50334143+JohannesWalter@users.noreply.github.com>
2020-08-13 10:41:40 +0200

    new structure

--- Linear_Regression.jl
+++ Linear_Regression.jl
@@ -5,10 +5,11 @@ This is my implementation of Linear Regression in Julia =#
 # Is there a request package in Julia?
 # Let's find out!
 
-# using Pkg 
+using Pkg 
 # Pkg.add("HTTP")
-using VegaLite, DataFrames, Query
+Pkg.add("DataFrames")
 using HTTP
+using DataFrames
 
 response = HTTP.request("GET", "https://raw.githubusercontent.com/yashLadha/The_Math_of_Intelligence/master/Week1/ADRvsRating.csv")
 
@@ -24,7 +25,18 @@ data = []
 lines = lines[2:length(lines)]
 
 lines[1]
+split(lines[1], ",")
+test = ["one, two", "three, four"]
+typeof(test)
+typeof(lines)
+test[1]
+lines[1]
+
+
+
+
 splitted_line = split(lines[1], ",")
+splitted_line
 splitted_line[1]
 typeof(Tuple(splitted_line))
 typeof(splitted_line[1])
@@ -35,8 +47,31 @@ typeof(z)
 
 for i in 1:length(lines)
     i = split(lines[i], ",")
-    append!(data, Tuple(i))
+    append!(data, i)
 end
 
+typeof(lines)
+typeof(data)
+typeof(data[1])
 
-data[1]
+1 % 2
+
+
+
+
+
+
+
+X = []
+Y = []
+for i in 1:length(data)
+    if i % 2 == 1
+        elem = parse(Float64, data[i])
+        append!(X, elem)
+    elseif i % 2 == 0
+        elem = parse(Float64, data[i])
+        append!(Y, elem)
+    end
+end
+
+df = DataFrame(X=X, Y=Y)
